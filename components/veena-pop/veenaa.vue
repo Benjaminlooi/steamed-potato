e533c1dda2f958cbf800529fddf62ecc07871bdf
benjaminlooi <benjaminlooidt@gmail.com>
2022-05-23 22:06:16 +0800

    New page: veenaa pop

---
+++ components/veena-pop/veenaa.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    class="veenaa-pop"
+    :style="{ top: veenaaPopItem.y + 'px', left: veenaaPopItem.x + 'px' }"
+  ></div>
+</template>
+
+<script lang="ts">
+import Vue, { PropOptions } from 'vue'
+import { Veenaa } from '~/pages/veenaa-pop.vue'
+
+export default Vue.extend({
+  props: {
+    veenaaPopItem: {
+      type: Object,
+      required: true,
+    } as PropOptions<Veenaa>,
+  },
+  mounted() {
+    setTimeout(() => this.$emit('expired', this.veenaaPopItem.id), 2000)
+  },
+})
+</script>
+
+<style lang="scss">
+.veenaa-pop {
+  position: absolute;
+  background: url('~assets/veenaa.PNG');
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  pointer-events: none;
+  transform: translate(-50%, -50%);
+  animation: animate 10s linear infinite;
+  width: 77.4427px;
+  height: 77.4427px;
+}
+
+@keyframes animate {
+  0% {
+    transform: translate(-50%, -50%);
+    opacity: 1;
+  }
+
+  100% {
+    transform: translate(-50%, -1000%);
+    opacity: 0;
+  }
+}
+</style>
---
+++ pages/veenaa-pop.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="h-screen w-screen" @mousemove="spawnVeenaa($event)">
+    <veenaa
+      v-for="veenaaPop in veenaaList"
+      :key="veenaaPop.id"
+      :veenaa-pop-item="veenaaPop"
+      @expired="removeVeenaa(veenaaPop.id)"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import veenaa from '../components/veena-pop/veenaa.vue'
+
+export declare interface Veenaa {
+  id: number
+  x: number
+  y: number
+}
+
+export default Vue.extend({
+  components: { veenaa },
+  data: () => ({
+    count: 0,
+    veenaaList: [] as Veenaa[],
+  }),
+  methods: {
+    spawnVeenaa(e: MouseEvent) {
+      this.veenaaList.push({ id: this.count++, x: e.x, y: e.y })
+    },
+    removeVeenaa(veenaaPopID: number) {
+      this.veenaaList.splice(
+        this.veenaaList.findIndex((veenaa) => veenaa.id === veenaaPopID),
+        1
+      )
+    },
+  },
+})
+</script>
+
+<style lang="scss"></style>
